685ffd39de8fbd6d3918d808b37cc23ceee8fe9e
Charlie Greenman <charlie@razroo.com>
2023-08-29 23:09:51 -0700

    integrate footer

--- apps/test-codegen-twelve/src/app/app.module.ts
+++ apps/test-codegen-twelve/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./routing/app-routing.module";
-import { GlobalHeaderComponentModule } from "@test-codegen-twelve/ui";
+import { GlobalHeaderComponentModule, GlobalSidenavModule } from "@test-codegen-twelve/ui";
 import { GlobalFooterModule } from "@test-codegen-twelve/ui";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule],
+  imports: [BrowserModule, BrowserAnimationsModule, 
+    AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule,
+    GlobalSidenavModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
